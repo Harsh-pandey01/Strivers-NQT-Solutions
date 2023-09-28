7c51df17b64fcdd47fb2df3d088c8d2d018fb3c2
Harsh-Pandey <harshcse7571@gmail.com>
2023-09-28 04:47:06 -0700

    Solved Q4 of Array

---
+++ ARRAYS/Q4.cpp
@@ -0,0 +1,25 @@
+// Reverse a given array
+// i/p :- {1,2,3,4,5,6}
+// o/p :- {6,5,4,3,2,1}
+
+
+                                                  //  Approach 1
+                                    // use inbuild reverse method to reverse the array
+
+// int arr[6]= {1,2,3,4,5,6};
+// reverse(arr,arr+6);
+
+// The complexity of the above soltuion is O(N);
+
+                                                //   Approach 2 
+                                             // Two pointer method
+
+// int start = 0 ;
+// int end = arr.size() -1 ;
+// while(start <= end){
+//     swap(arr[start],arr[end]);
+//     start ++ ;
+//     end --;
+// }
+
+// The complexity of the above soltuion is O(N);
